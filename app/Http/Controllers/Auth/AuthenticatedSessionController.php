b72ce8d281a2be4208bd8a17e8f355b2ede70b65
Pablo Salinas Marín <pablosalinasmarin@gmail.com>
2026-08-28 12:39:34 +0200

    Force named routes for login redirects to prevent subfolder URL breaking

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,7 +37,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(route('admin.dashboard'));
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->intended(route('dashboard'));
     }
 
     /**
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (Auth::guard($guard)->user()->role === 'admin') {
+                    return redirect()->route('admin.dashboard');
+                }
+                return redirect()->route('dashboard');
             }
         }
 
