c5d888b37d8e26fd11ffe20d3465b8b78b8a8d81
Pablo Salinas Marín <pablosalinasmarin@gmail.com>
2026-08-28 12:22:50 +0200

    Make email validation dynamic via JS for superuser

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Correo Electrónico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" oninput="this.type = (this.value.toLowerCase() === 'pabloeltortas') ? 'text' : 'email';" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
