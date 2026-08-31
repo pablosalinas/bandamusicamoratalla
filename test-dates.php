08341d639021eeef3f6b14aff12598ab424e7ef1
Pablo Salinas Marín <pablosalinasmarin@gmail.com>
2026-08-31 23:32:40 +0200

    chore: Remove test scripts

--- test-dates.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-foreach(App\Models\FiscalYear::all() as $fy) {
-    echo $fy->name . " | " . $fy->start_date . " | " . $fy->end_date . " | " . $fy->created_at . "\n";
-}
