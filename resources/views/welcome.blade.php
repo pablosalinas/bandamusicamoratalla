0ffdbe15095628918503ea3fbfea4f45bcd20d69
Pablo Salinas Marín <pablosalinasmarin@gmail.com>
2026-09-01 14:23:50 +0200

    Add custom favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Banda de Música de Moratalla</title>
     
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     <!-- SEO Meta Tags -->
     <meta name="description" content="Página oficial de la Banda de Música de Moratalla. Conoce nuestra historia, descubre a nuestros músicos y mantente al día de las últimas noticias y actuaciones.">
     
--- test_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-foreach (App\Models\SiteSetting::all() as $s) {
-    echo $s->key . ' = ' . $s->value . "\n";
-}
