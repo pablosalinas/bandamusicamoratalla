4c45cb4307046a7b1b786ba82ffef8f25e56b8fa
Pablo Salinas Marín <pablosalinasmarin@gmail.com>
2026-09-15 00:00:27 +0200

    Solucionar duplicado de saxo baritono en propuestas aplicando canónico

--- resources/views/admin/sheet-music/edit.blade.php
+++ resources/views/admin/sheet-music/edit.blade.php
@@ -477,7 +477,10 @@
                                 for(let m of matchedInstrumentsArr) {
                                     let mNorm = m.originalName.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
                                     let pNorm = part.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-                                    if(mNorm.includes(pNorm) || pNorm.includes(mNorm)) {
+                                    let canonicalM = mNorm.replace(/saxofones/g, 'saxos').replace(/saxofon/g, 'saxo').replace(/eufonio|euphonium|bombardino/g, 'eufonium').replace(/\btuba\b/g, 'bajo');
+                                    let canonicalP = pNorm.replace(/saxofones/g, 'saxos').replace(/saxofon/g, 'saxo').replace(/eufonio|euphonium|bombardino/g, 'eufonium').replace(/\btuba\b/g, 'bajo');
+                                    
+                                    if(canonicalM.includes(canonicalP) || canonicalP.includes(canonicalM)) {
                                         isAlreadyMatched = true; 
                                         break;
                                     }
